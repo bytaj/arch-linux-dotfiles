b201a1f28884694924fcd39c9f87b8071526ac0b
bytaj <theantoniojose97@gmail.com>
2021-10-31 18:23:27 +0100

    previus changes

---
+++ installation/install_dependencies.sh
@@ -0,0 +1,6 @@
+sudo pacman -S --noconfirm sed which binutils python-pip udiskie thunar pulseaudio pavucontrol brightnessctl xorg-xinit \
+rofi feh picom volumeicon
+pip install psutil
+touch $HOME/.xsession
+chmod u+x $HOME/.xsession
+yay -SYg nerd-fonts-ubuntu-mono
